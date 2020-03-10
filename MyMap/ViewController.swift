cc52cd9b7a36b5616cba3c9d089a3fff6581c3d7
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-03-11 08:24:15 +0900

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by 菅野魁斗 on 2020/03/11.
+//  Copyright © 2020 kaito.sugano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
